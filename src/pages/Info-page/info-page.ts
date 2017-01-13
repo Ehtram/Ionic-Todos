71253139a4d479e88ccf896c1c81cea41d8bbc2b
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-01-13 08:23:58 +0100

    Initial Commit

---
+++ src/pages/Info-page/info-page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-page',
+  templateUrl: 'info-page.html'
+})
+export class InfoComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
